2c104e2b3c3bb49459a1b8f17c134845dabfd44f
Andrew Caya <andrewscaya@yahoo.ca>
2021-07-26 18:44:02 -0400

    2021-07-26 AC: Updates the footer section.

--- templates/plates_bootstrap/footer.php
+++ templates/plates_bootstrap/footer.php
@@ -8,8 +8,8 @@
                         <li class="wow animated zoomIn"><a href="https://facebook.com/asclinux" target="_blank"><i class="fa fa-facebook fa-3x"></i></a></li>
                         <li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="https://twitter.com/linuxforphp" target="_blank"><i class="fa fa-twitter fa-3x"></i></a></li>
                         <li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="https://www.youtube.com/channel/UCrNl2Vd5SABy7T5nWAHxR5A" target="_blank"><i class="fa fa-youtube fa-3x"></i></a></li>
-                        <li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="https://hub.docker.com/u/asclinux/" target="_blank"><i class="fa fa-cloud-download fa-3x"></i></a></li>
-                        <li class="wow animated zoomIn" data-wow-delay="1.2s"><a href="https://github.com/linuxforphp" target="_blank"><i class="fa fa-code fa-3x"></i></a></li>
+                        <li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="https://hub.docker.com/r/asclinux/flarelinux" target="_blank"><i class="fa fa-cloud-download fa-3x"></i></a></li>
+                        <li class="wow animated zoomIn" data-wow-delay="1.2s"><a href="https://github.com/linuxforphp/flarelinux" target="_blank"><i class="fa fa-code fa-3x"></i></a></li>
                     </ul>
                 </div>
 
