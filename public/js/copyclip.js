990bda68b4bf3c6f735fc8cd735e4acb603e7e29
Andrew Caya <andrewscaya@yahoo.ca>
2024-11-18 17:26:43 -0500

    2024-11-18 AC: Fixes an issue with a JS listener.

--- public/js/copyclip.js
+++ public/js/copyclip.js
@@ -13,10 +13,6 @@ document.querySelector('#myDockerCommand1Button').onclick = function() {
     copyClip('myDockerCommand1Button', 'docker run -dit --restart=always -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.1.5-1.9.0-0.7.0 flare --flare-coston2');
 }
 
-document.querySelector('#myDockerCommand2Button').onclick = function() {
-    copyClip('myDockerCommand2Button', 'docker exec -it my_flare_server /bin/bash');
-}
-
 document.querySelector('#myDockerCommand6Button').onclick = function() {
     copyClip('myDockerCommand6Button', 'docker rm -f my_flare_server');
 }
